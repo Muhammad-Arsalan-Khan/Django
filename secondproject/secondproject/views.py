007b4e2cd5f34bf9b5bae253cc96b2113103fb98
Muhammad-Arsalan-Khan <arsalan.debug@gmail.com>
2026-02-22 02:32:38 +0500

    second

---
+++ secondproject/secondproject/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, World!")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("This is the about page.")  
+    return render(request, 'innner/about.html')
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
